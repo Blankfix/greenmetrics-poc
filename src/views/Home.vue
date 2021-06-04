5429e4dddf69f4f021a0fe74f16a6f052a4b9c0b
cyril.deconinck@gmail.com <gengen5891>
2021-06-04 11:24:19 +0200

    Initial commit : - Vue.js - Vue rooter - Vuetify - pug & scss

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  <the-footer />
+</template>
+
+<script>
+  //import HelloWorld from '../components/HelloWorld'
+  import TheFooter from '../components/TheFooter'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      TheFooter,
+    },
+  }
+</script>
